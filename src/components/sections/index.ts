6f746241e52e024b07aa1dc57f6cea3b19950fa5
Andreu Garcia <andreu.garcia@avoristravel.com>
2026-05-29 13:06:34 +0200

    #main feat: re-export diagram types from sections barrel

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,4 @@
 export { SectionProvider, useSections } from './SectionContext'
 export type { SectionConfig, SectionInput, SectionTheme, TabVariant, SectionsContextValue } from './SectionContext'
 export { Block } from './Block'
+export type { DiagramJSON, DiagramNodeDef, DiagramEdgeDef } from './diagram/types'
